a544c69d3871e03031295e3fadd99111f231a247
Arturo PC <arturopc_acl@hotmail.com>
2026-06-28 22:15:46 -0600

    Fix: Parche definitivo de CORS y columnas analiticas

--- server.js
+++ server.js
@@ -250,3 +250,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor backend escuchando exitosamente en el puerto ${PORT}`);
 });
+
+// Forzar compilacion definitiva junio 2026
